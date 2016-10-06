85ca8c35ba0c51c683cba31664d69609dd1c70e4
Miglen <github@miglen.com>
2016-10-06 08:48:36 +0100

    Update of regex patters into separate file
    I have updated the regex into a separate file regex.php which I'm using to parse all pages when in multicomplete or submit modes.

--- files/php/submit.php
+++ files/php/submit.php
@@ -4,92 +4,93 @@
 	if(isset($_GET['check'])){
 
 		$result = $mysqli->query("SELECT * FROM `solved` WHERE `found`<`questions`;") or die($mysqli->error.__LINE__);
-				
+
 		while($item = $result->fetch_assoc()){
-		
+
 			$result2 = $mysqli->query("SELECT `asid` FROM `html` WHERE `asid`='".$item['asid']."'") or die($mysqli->error.__LINE__);
 			if(mysqli_num_rows($result2)<1){
 				preg_match_all('/<form name="aspnetForm" method="post" action="Assessment.aspx\?data=(.*?)" id="aspnetForm">/si', $item['html'], $qstring);
 				echo "<a href='#' target='_blank'>Asid: ".$item['asid']."</a><br><br>";
 			}
-		}		
+		}
 		exit;
-		
+
 	}
 
 
 
+
 	if(!empty($_POST) && isset($_POST['data']) && !empty($_POST['data']) && strlen($_POST['data'])>100 ){
-	
+
 
 		$data = $_POST['data'];
 	//echo strlen($data);
 	$data = explode('<table width="100%" border="0" cellpadding="2" cellspacing="5">',$data);
-	//echo "<br>".strlen($data[1]);	
+	//echo "<br>".strlen($data[1]);
 	$data = explode('<input type="submit" name="ctl00$cphMainBodyContainer$btnResultsBottom" value="Results"',$data[1]);
 	$data = $data[0];
-	
+
 		$danni = array();
 
-		preg_match_all('/<span id="cphMainBodyContainer_lblExamineeName" class="LabelText">(.*?)<\/span>/s', $data, $matches);
+		preg_match_all($regex_pmpi['examinee_name'], $data, $matches);
 		$danni['name'] = $matches[1][0];
 
-		preg_match_all('/<span id="cphMainBodyContainer_lblAssessmentName" class="LabelText">(.*?)<\/span>/s', $data, $matches);
+		preg_match_all($regex_pmpi['exam_name'], $data, $matches);
 		$danni['assessment'] = $matches[1][0];
 
-		preg_match_all('/<span id="cphMainBodyContainer_lblAssessmentID" class="LabelText">(\d+)<\/span>/s', $data, $matches);
+		preg_match_all($regex_pmpi['exam_id'], $data, $matches);
 		$danni['id'] = intval($matches[1][0]);
-		
+
 		//Sections parser (all subtypes)
-		preg_match_all('/<span id="cphMainBodyContainer_ctl.._lblSectionName">(.*?)<\/span>/si', $data, $techz);
+		preg_match_all($regex_pmpi['exam_sections'], $data, $techz);
 		$subtypes=array();
-			
+
 			foreach($techz[1] as $k=>$v){
 			$v = trim($v);
 				if(!in_array($v, $subtypes, true) && $v!="Label"){
 					array_push($subtypes, $v);
 				}
 			}
-		
+
 		if(sizeof($subtypes)!=0){
-			
+
 		}else $danni['sections'] = "";
-			
-		
+
+
 
 		preg_match_all('/\((.*?)\)/s', $danni['assessment'], $matches);
 		#print_r($matches);
 		$danni['date'] = end($matches[1]);
 
 		$danni['assessment'] = str_replace(" (".$danni['date'].")",null,$danni['assessment']);
-		
+
 		$query = "SELECT `id` FROM `html` WHERE `asid`='".$danni['id']."' LIMIT 1;";
-		
+
 		$row = $mysqli->query($query) or die($mysqli->error.__LINE__);
 		$row = $row->fetch_assoc();
-		
-		
-		
+
+
+
 			if(strlen($data)>100 && $danni['id'] > 100 && !isset($row['id'])){
 			//print_r($danni);
 			//exit;
-				
 
-				
+
+
 				$query = "INSERT INTO `html` (`html`,`asid`,`name`,`date_taken`,`assessment`) VALUES ('".$mysqli->real_escape_string($data)."','".$danni['id']."','".$mysqli->real_escape_string($danni['name'])."','".$mysqli->real_escape_string($danni['date'])."','".$mysqli->real_escape_string($danni['assessment'])."');";
 				$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-				
+
 				echo '<h1><a href="#" class="header-anchor">#</a>Thank you for feeding me!</h1>';
-				
-				
+
+
 				parseHTML2DB($data,$mysqli->insert_id);
-				
+
 				exit;
 			}else{
 				echo '<h1><a href="#" class="header-anchor">#</a>What was that? Sorry I don\'t eat plankton, I need HTML!</h1>';
 				exit;
 			}
-	
+
 	}
 
 ?>
@@ -99,7 +100,7 @@
 
 
 		<textarea style="width:100%;height:300px;" name="data"></textarea>
-		
+
 	    <input type="submit" value="Feed the Shark!" style="margin-bottom: 4px;border-radius: 4px;background: #00ACEE;color:#fff;padding:10px;font-weight:bold;">
 		</form>
 <br><br><br>
