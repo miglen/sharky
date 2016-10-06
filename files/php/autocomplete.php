7b1c21ac644016a2e0be0b32ac845e0755956b8b
Miglen <github@miglen.com>
2016-10-06 08:48:36 +0100

    Update of regex patters into separate file
    I have updated the regex into a separate file regex.php which I'm using to parse all pages when in multicomplete or submit modes.

--- files/php/autocomplete.php
+++ files/php/autocomplete.php
@@ -1,12 +1,8 @@
-<?php
+<h2 id="bowerjson"><a href="#" class="header-anchor">#</a>AutoComplete</h2>
+  <div id="searchfield">
+      <form><input type="text" name="currency" class="biginput" id="autocomplete"></form>
+    </div><!-- @end #searchfield -->
 
-?>
-
-        <h2 id="bowerjson"><a href="#" class="header-anchor">#</a>AutoComplete</h2>
-		      <div id="searchfield">
-        <form><input type="text" name="currency" class="biginput" id="autocomplete"></form>
-      </div><!-- @end #searchfield -->
-		
-	  <div id="outputbox">
+    <div id="outputbox">
 				<p id="outputcontent">Stay tuned! Result will appear here if you type something I know.</p>
-      </div>
+    </div>
